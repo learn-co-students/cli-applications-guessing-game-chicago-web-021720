d28ec27a7e95476b6adfdece9c68809d5114733b
cerebral-caller-0985 <ashaffe@gmail.com>
2020-02-02 22:01:51 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,7 @@
 # Code your solution here!
+def run_guessing_game
+  magicnumber = (rand(6) + 1).to_s
+  puts "Guess a number between 1 and 6"
+  userguess = gets.chomp
+  puts userguess == magicnumber ? "You guessed the correct number!" : userguess == "exit" ? "Goodbye!" : "Sorry! The computer guessed #{magicnumber}."
+end
